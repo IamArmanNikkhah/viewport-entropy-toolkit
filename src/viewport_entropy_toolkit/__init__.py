59aa94f5d8b5e50b266b2afe1d3ac618a2d72e79
Arman Nik Khah <rmnnikkhah@gmail.com>
2024-12-20 11:05:25 -0600

    Fix: Fixed importing in the init of main for data_types

--- src/viewport_entropy_toolkit/__init__.py
+++ src/viewport_entropy_toolkit/__init__.py
@@ -4,7 +4,7 @@ This package provides tools for analyzing spatial entropy in 360-degree video vi
 It includes functionality for data processing, entropy calculation, and visualization.
 """
 
-from .data_types import Point, RadialPoint, Vector, ValidationError, SpatialError
+from .data_types import Point, RadialPoint, Vector, ValidationError, SpatialError, convert_vectors_to_coordinates
 from .config import AnalyzerConfig, DEFAULT_VIDEO_DIMENSIONS, DEFAULT_TILE_COUNTS
 from viewport_entropy_toolkit.analyzers import SpatialEntropyAnalyzer
 
@@ -16,6 +16,7 @@ __all__ = [
     'Vector',
     'ValidationError',
     'SpatialError',
+    'convert_vectors_to_coordinates',
     'AnalyzerConfig',
     'SpatialEntropyAnalyzer',
     'DEFAULT_VIDEO_DIMENSIONS',
