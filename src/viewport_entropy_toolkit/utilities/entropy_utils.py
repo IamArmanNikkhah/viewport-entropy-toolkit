7cb5cce65a5c6950445a2d4f97d4ffd152aed171
Chtun <chtun@live.com>
2025-02-07 15:26:45 -0600

    Fix on find naive tile index.

--- src/viewport_entropy_toolkit/utilities/entropy_utils.py
+++ src/viewport_entropy_toolkit/utilities/entropy_utils.py
@@ -410,8 +410,8 @@ def find_naive_tile_index(
         Dict[Vector, float]: Dictionary mapping tile centers to weights.
     """
     # calculate the tile this vector sits in.
-    tile_lon_index = int(point.lon + 180 / tile_width)
-    tile_lat_index = int(point.lat + 90 / tile_height)
+    tile_lon_index = int((point.lon + 180) / tile_width)
+    tile_lat_index = int((point.lat + 90) / tile_height)
 
     return f"{tile_lon_index}_{tile_lat_index}"
 
