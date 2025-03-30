7fc8283523e5adf38cb635bfc31b302fd67898a1
Chtun <chtun@live.com>
2025-03-30 15:50:39 -0500

    Added debug statement.

--- src/viewport_entropy_toolkit/utilities/data_utils.py
+++ src/viewport_entropy_toolkit/utilities/data_utils.py
@@ -841,7 +841,8 @@ def compute_FB_tile_areas(tile_count: int) -> Tuple[Dict[int, float], Dict[int, 
     furthest_search_factor = 1.7
 
     if (tile_count > 2000):
-        furthest_search_factor = 1.6
+        furthest_search_factor = 1.5
+        print(f"Using a separate furthest_search_factor: {furthest_search_factor}")
 
     tile_boundaries_dict = get_FB_tile_boundaries(tile_count, furthest_search_factor)
 
